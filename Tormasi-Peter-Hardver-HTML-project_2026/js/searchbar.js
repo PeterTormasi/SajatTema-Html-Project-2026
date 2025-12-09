5ba82ddcdb27223ab4eab9f49e802a366f2c1aff
Szotyi <peti.tormasi@gmail.com>
2025-12-09 19:10:36 +0100

    Add files via upload

--- Tormasi-Peter-Hardver-HTML-project_2026/js/searchbar.js
+++ Tormasi-Peter-Hardver-HTML-project_2026/js/searchbar.js
@@ -4,18 +4,17 @@ const headbanner = document.getElementById("banner-content");
 const header = document.getElementById("main-header");
 
 window.addEventListener("scroll", () => {
-    console.log(searchbox.classList)
     if (window.scrollY > 50) {
-        searchbox.style.transform = "translateX(0px)"
-        searchbox.style.opacity = "1"
-        headbanner.style.display = "none"
+        searchbox.style.opacity = "1" 
         header.style.height = "50px"
         header.style.marginBottom = "50px"
+        headbanner.style.pointerEvents = "None"
+        headbanner.style.opacity = "0"
     } else {
-        searchbox.style.transform = "translateX(20000px)"
         searchbox.style.opacity = "0"
-        headbanner.style.display = "flex"
         header.style.height = "100px"
         header.style.marginBottom = "0px"
+        headbanner.style.pointerEvents = "All"
+        headbanner.style.opacity = "1"
     }
 });
